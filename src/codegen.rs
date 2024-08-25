1ca291b8d0d9de31378b877b079e6a0ce72e5b65
clflushopt <172141496+clflushopt@users.noreply.github.com>
2024-08-25 16:02:12 -0400

    fix: Push some protoype runtime codegen code

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,4 +1,6 @@
 //! Implementation of runtime code generation for query execution.
+use crate::row::{self, Int64Row};
+use dynasmrt::{aarch64::Assembler, dynasm, AssemblyOffset, DynasmApi, ExecutableBuffer};
 
 /// Index of the column in a row, used as an alias for the column name.
 type ColumnIndex = usize;
@@ -43,6 +45,9 @@ struct QueryPlan {
     pipeline: Vec<Operator>,
 }
 
+#[derive(Debug)]
+struct CompiledQueryPlan(AssemblyOffset, ExecutableBuffer);
+
 impl QueryPlan {
     /// Create a new query plan.
     fn new() -> Self {
@@ -55,32 +60,65 @@ impl QueryPlan {
     fn push(&mut self, operator: Operator) {
         self.pipeline.push(operator)
     }
+}
 
-    /// Compile the query plan to a native code buffer.
-    fn compile(&self) {
-        todo!("Unimplemented query compilation !")
+struct QueryCompiler {
+    plan: QueryPlan,
+    rows: Vec<Int64Row>,
+}
+
+impl QueryCompiler {
+    /// Create a new query compiler.
+    fn new(plan: QueryPlan, rows: Vec<Int64Row>) -> Self {
+        Self { plan, rows }
+    }
+
+    /// Compile the query plan and return a compiled query plan which is a tuple
+    /// of an entry point and executable machine code.
+    fn compile(&self) -> CompiledQueryPlan {
+        // Create a new assembler.
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+        let mut entry_point = assembler.offset();
+
+        CompiledQueryPlan(entry_point, assembler.finalize().unwrap())
+    }
+
+    /// Compile the `scan` operator.
+    fn scan(&mut self, assembler: &mut Assembler) {
+        let entry_point = assembler.offset();
+
+        // The rows are assumed to be packed, I guess.
+        let row_data = self.rows.as_mut_ptr();
+        let row_count = self.rows.len();
+
+        // Scan is the entry point of the pipeline, which means all downstream
+        // operators end up calling it.
+        dynasm!(assembler
+            ; .arch x64
+            ; push rbp
+            ; mov rbp, rsp
+            ; mov rdi, QWORD row_data as _
+            ; mov rcx, row_count as _
+        );
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use dynasmrt::{dynasm, DynasmApi, DynasmLabelApi, ExecutableBuffer};
+    use dynasmrt::{dynasm, AssemblyOffset, DynasmApi, DynasmLabelApi, ExecutableBuffer};
 
-    pub struct JitCompiler {
-        assembler: dynasmrt::x64::Assembler,
-    }
+    pub struct JitCompiler {}
 
     impl JitCompiler {
         pub fn new() -> Self {
-            JitCompiler {
-                assembler: dynasmrt::x64::Assembler::new().unwrap(),
-            }
+            JitCompiler {}
         }
 
-        pub fn compile_filter(&mut self) -> ExecutableBuffer {
-            let entry_point = self.assembler.offset();
+        pub fn compile_filter(&mut self) -> (AssemblyOffset, ExecutableBuffer) {
+            let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+            let entry_point = assembler.offset();
 
-            dynasm!(self.assembler
+            dynasm!(assembler
                 ; .arch x64
                 // Prologue: setting up the stack frame
                 ; push rbp
@@ -109,13 +147,13 @@ mod tests {
                 ; ret
             );
 
-            self.assembler.finalize().unwrap()
+            (entry_point, assembler.finalize().unwrap())
         }
     }
-
-    fn main() {
+    #[test]
+    fn can_compile_basic_filter() {
         let mut compiler = JitCompiler::new();
-        let filter_fn = compiler.compile_filter();
+        let (entry_point, filter_fn) = compiler.compile_filter();
 
         let rows = vec![vec![1, 2, 3, 10000], vec![1, 2, 3, 8000]];
 
@@ -126,5 +164,125 @@ mod tests {
             let result = filter(row.as_ptr());
             println!("Row: {:?}, Passed: {}", row, result == 1);
         }
+    }
+}
+
+#[cfg(test)]
+mod scan_tests {
+    use dynasmrt::{dynasm, AssemblyOffset, DynasmApi, DynasmLabelApi, ExecutableBuffer};
+
+    pub struct JitCompiler {
+        row_data: *const i64,
+        row_count: usize,
+    }
+
+    impl JitCompiler {
+        pub fn new(row_data: *const i64, row_count: usize) -> Self {
+            JitCompiler {
+                row_data,
+                row_count,
+            }
+        }
+
+        pub fn compile_project(
+            &mut self,
+            column: usize,
+            data: &mut [i64],
+        ) -> (AssemblyOffset, ExecutableBuffer) {
+            let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+            let entry_point = assembler.offset();
+            let data_ptr = data.as_ptr();
+            let stride_size = self.row_count / 4;
+            dynasm!(assembler
+                ; .arch x64
+                // Load src vector.
+                ; mov rsi, QWORD self.row_data as _
+                // Load dst vector.
+                ; mov rdi, QWORD data_ptr as _
+                // Initialize index (RCX) to 0
+                ; mov rcx, 0
+                ; ->loop_start:
+                // Compare index with data length
+                ; cmp rcx, stride_size as i64 as i32
+                // If index >= length, exit loop
+                ; jge >exit
+                // Save row index in RBX.
+                // Copy from the projected column from src to dst
+                ; mov rax, [rsi + column as i64 as i32  * 8]
+                ; mov [rdi + rcx * 8], rax
+                // Increment index into `src`.
+                // Increment index into `dst`.
+                ; inc rcx
+                // Repeat.
+                ; jmp ->loop_start
+                ; exit:
+                ; ret
+            );
+
+            let buffer = assembler.finalize().unwrap();
+
+            (entry_point, buffer)
+        }
+
+        pub fn compile_scan(&mut self, data: &mut [i64]) -> (AssemblyOffset, ExecutableBuffer) {
+            let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+            let entry_point = assembler.offset();
+            let data_ptr = data.as_ptr();
+
+            dynasm!(assembler
+                ; .arch x64
+                ; mov rsi, QWORD self.row_data as _ // Load src vector.
+                ; mov rdi, QWORD data_ptr as _ // Load dst vector.
+                ; mov rcx, 0                                // Initialize index (RSI) to 0
+                ; ->loop_start:
+                ; cmp rcx, self.row_count as i64 as i32       // Compare index with data length
+                ; jge >exit                                 // If index >= length, exit loop
+                ; mov rax, [rsi + rcx * 8]                  // Copy from src to dst
+                ; mov [rdi + rcx * 8], rax
+                 // Here, you can add instructions to process each row
+                ; add rcx, 1                                // Increment index
+                ; jmp ->loop_start                          // Repeat loop
+                ; exit:
+                ; ret                                       // Return from function
+            );
+
+            let buffer = assembler.finalize().unwrap();
+
+            (entry_point, buffer)
+        }
+    }
+
+    #[test]
+    fn can_build_scan_pipeline() {
+        let rows = vec![
+            vec![1, 2, 3, 4000],
+            vec![1, 2, 3, 8000],
+            vec![1, 2, 3, 12000],
+        ];
+
+        // Flatten the rows into a single buffer
+        let flat_rows: Vec<i64> = rows.into_iter().flatten().collect();
+
+        let mut compiler = JitCompiler::new(flat_rows.as_ptr(), flat_rows.len());
+        let mut data = vec![0; flat_rows.len()];
+        let (entry_point, buffer) = compiler.compile_scan(data.as_mut_slice());
+
+        println!("Entry point: {:?}", entry_point);
+
+        // Execute the compiled code
+        let exec_fn: extern "C" fn() -> () =
+            unsafe { std::mem::transmute(buffer.ptr(entry_point)) };
+        exec_fn();
+        println!("Data: {:?}", data);
+
+        let mut data = vec![0; 4];
+        let (entry_point, buffer) = compiler.compile_project(3, data.as_mut_slice());
+
+        println!("Entry point: {:?}", entry_point);
+        // Execute the compiled code
+        let exec_fn: extern "C" fn() -> () =
+            unsafe { std::mem::transmute(buffer.ptr(entry_point)) };
+        exec_fn();
+        println!("Data: {:?}", data)
     }
 }
--- src/row.rs
+++ src/row.rs
@@ -1,4 +1,4 @@
-//! Implementation of in-memory rows, represented as `Vec<String>`.
+//! Implementation of in-memory generic and simple rows, represented as `Vec<String>`.
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Row {
@@ -16,5 +16,28 @@ impl Row {
     /// Returns item at given index.
     pub fn get(&self, index: usize) -> Option<&str> {
         self.items.get(index).map(|x| x.as_str())
+    }
+}
+
+/// Rows that only hold `i64` tuple, because dealing with `String` in inline
+/// assembly might be a pita we just assume all columns are numbers, certainly
+/// all important databases grown up use have `i64`. You woulnd't put a `f64`
+/// in there would you now ?
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Int64Row {
+    pub items: Vec<i64>,
+}
+
+impl Int64Row {
+    /// Create a new row from a slice of `i64`.
+    pub fn new(items: &[i64]) -> Self {
+        Self {
+            items: items.to_vec(),
+        }
+    }
+
+    /// Return item at given index without bound checks.
+    pub fn get(&self, index: usize) -> i64 {
+        *self.items.get(index).unwrap()
     }
 }
